b052b93197afa9c9206eb8b4d9945575a6d2b822
Markell Long <nmcc1108@gmail.com>
2024-05-02 17:59:00 -0600

    inital files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react'
+import {Amplify, API} from "aws-amplify";
+import awsExports from "./aws-exports";
+Amplify.configure(awsExports);
 
-function App() {
+const myAPI = "api074336c8"
+const path = '/customers'; 
+
+const App = () => {
+  const [input, setInput] = useState("")
+  const [customers, setCustomers] = useState([])
+
+  //Function to fetch from our backend and update customers array
+  function getCustomer(e) {
+    let customerId = e.input
+    API.get(myAPI, path + "/" + customerId)
+       .then(response => {
+         console.log(response)
+         let newCustomers = [...customers]
+         newCustomers.push(response)
+         setCustomers(newCustomers)
+
+       })
+       .catch(error => {
+         console.log(error)
+       })
+  }
+
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>CS506 Final Customer Input</h1>
+      <div>
+          <input placeholder="customer id" type="text" value={input} onChange={(e) => setInput(e.target.value)}/>      
+      </div>
+      <br/>
+      <button onClick={() => getCustomer({input})}>Get Customer From Backend</button>
+
+      <h2 style={{visibility: customers.length > 0 ? 'visible' : 'hidden' }}>Response</h2>
+      {
+       customers.map((thisCustomer, index) => {
+         return (
+        <div key={thisCustomer.customerId}>
+          <span><b>CustomerId:</b> {thisCustomer.customerId} - <b>CustomerName</b>: {thisCustomer.customerName}</span>
+        </div>)
+       })
+      }
     </div>
-  );
+  )
 }
 
 export default App;
